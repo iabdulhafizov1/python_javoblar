b3c80ceebb2a20331cdcca92308b7c0ad2bf91c5
iabdulhafizov1 <ibrohimabdulhafizov100@gmail.com>
2025-03-21 16:53:19 +0500

    3-dars topshiriqlari bajarildi
    3-darsdagi topshiriqlar mustaqil bajarildi!

---
+++ 3-dars_topshiriq_javoblar.py
@@ -0,0 +1,15 @@
+#1-topshiriq
+print('5 ning 4-dasrajasi: ',5**4)
+
+#2-topshiriq
+print('22 ni 4 ga bo\'lgandagi qoldiq: ',22//4)
+
+#3-topshiriq
+print("Tomonlari 125 ga teng kvadrat yuzi: ",125*125)
+print("Tomonlari 125 ga teng kvadrat perimetri: ",4*125)
+
+#4-topshiriq
+print("Diametri 12 ga teng bo'lgan doira yuzi: ",(12/2)**2*3.14)
+
+#5-topshiriq
+print("Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchak gipotenuzasi: ",(6**2+7**2)**(1/2))

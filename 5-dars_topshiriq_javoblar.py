0f0e8eb03706df97b9bb347a58d88708bdd08f39
iabdulhafizov1 <ibrohimabdulhafizov100@gmail.com>
2025-03-23 10:42:12 +0500

    5-dars topshiriqlari
    5-dars topshiriqlari bajarildi

---
+++ 5-dars_topshiriq_javoblar.py
@@ -0,0 +1,73 @@
+#1-topshiriq
+kocha="Bog'bon"
+mahalla="sog'bon"
+tuman="Bodomzor"
+viloyat="Samarqand"
+
+#2-topshiriq
+kocha="Bog'bon"
+mahalla="sog'bon"
+tuman="Bodomzor"
+viloyat="Samarqand"
+print(f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati")
+'''
+Natija:
+Bog'bon ko'chasi, sog'bon mahallasi, Bodomzor tumani, Samarqand viloyati
+'''
+
+#3-topshiriq
+kocha=input("Ko'changizni nomini kiriting: ")
+mahalla=input("Mahallangiz nomini kiriting: ")
+tuman=input("Tug'ilgan shahar yoki tumaningiz nomini kiriting: ")
+viloyat=input("Viloyatingiz nomini kiriting: ")
+print(f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati")
+'''
+Natija:
+Ko'changizni nomini kiriting: Ezgulik
+Mahallangiz nomini kiriting: Ezgulik
+Tug'ilgan shahar yoki tumaningiz nomini kiriting: Quvasoy
+Viloyatingiz nomini kiriting: Farg'ona
+Ezgulik ko'chasi, Ezgulik mahallasi, Quvasoy tumani, Farg'ona viloyati
+'''
+
+#4-topshiriq
+kocha="Bog'bon"
+mahalla="sog'bon"
+tuman="Bodomzor"
+viloyat="Samarqand"
+print(kocha+" ko'chasi,\n"+mahalla+" mahallasi,\n"+
+      tuman+" tumani,\n"+viloyat+" viloyati")
+'''
+Natija:
+Bog'bon ko'chasi,
+sog'bon mahallasi,
+Bodomzor tumani,
+Samarqand viloyati
+'''
+
+#5-topshiriq
+kocha="Bog'bon"
+mahalla="sog'bon"
+tuman="Bodomzor"
+viloyat="Samarqand"
+manzil=f"{kocha} ko'chasi,{mahalla} mahallasi,\
+{tuman} tumani, {viloyat} viloyati"
+
+#6-topshiriq
+kocha="Bog'bon"
+mahalla="sog'bon"
+tuman="Bodomzor"
+viloyat="Samarqand"
+manzil=f"{kocha} ko'chasi,{mahalla} mahallasi,\
+{tuman} tumani, {viloyat} viloyati"
+print(manzil.title())
+print(manzil.upper())
+print(manzil.lower())
+print(manzil.capitalize())
+'''
+natija:
+Bog'Bon Ko'Chasi,Sog'Bon Mahallasi,Bodomzor Tumani, Samarqand Viloyati
+BOG'BON KO'CHASI,SOG'BON MAHALLASI,BODOMZOR TUMANI, SAMARQAND VILOYATI
+bog'bon ko'chasi,sog'bon mahallasi,bodomzor tumani, samarqand viloyati
+Bog'bon ko'chasi,sog'bon mahallasi,bodomzor tumani, samarqand viloyati
+'''

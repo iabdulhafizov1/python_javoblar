7f50dc8ae9681db9203974dcd82982485cd31569
iabdulhafizov1 <ibrohimabdulhafizov100@gmail.com>
2025-03-23 08:55:17 +0500

    4-dars topshiriq javoblari
    4-dars topshiriq javoblari bajarildi

---
+++ 4-dars_topshiriq_javoblar.py
@@ -0,0 +1,24 @@
+#1-topshiriq
+salom='Hello, World!'
+print(salom)
+#Hello, World!
+
+#2-topshiriq
+xabar='Salom, dunyo!'
+print(xabar)
+#Salom, dunyo!
+xabar='Bu python 3.7.0'
+print(xabar)
+#Bu python 3.7.0
+
+#3-topshiriq
+class=24
+print(class)
+#Xatolik turi: invalid syntax
+
+#4-topshiriq
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi * radius**2
+print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
+#Radiusi 5 ga teng aylananing yuzi= 78.53975
